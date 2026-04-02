4234729de2d1ea5b900ccf84f42b599090ef8fe0
mangaraju koppula <lakshmidurgaindana55@gmail.com>
2026-04-02 19:37:39 +0530

    Committing code and example

---
+++ code.py
@@ -0,0 +1,5 @@
+Strong experience in Java backend development
+• Hands-on experience with REST APIs and microservices architecture
+• Experience with frameworks like Spring / Spring Boot
+• Good understanding of databases (SQL/NoSQL)
+• Knowledge of version control tools like Git
